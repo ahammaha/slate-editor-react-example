b6453ce83a559151a344491ea6d47e4f76c5318c
ahammaha <mahalakshmi@outlook.com>
2018-12-18 12:06:50 +0530

    css changes

--- src/component/FileAttachment.js
+++ src/component/FileAttachment.js
@@ -1,15 +1,20 @@
 import React from 'react';
 
-const FileAttachment = (props) =>{
-	return(
-		<div className="file-attachment-div">
-			<a readOnly className="file-attachment" href={props.src}>
-				<span className="filename">{props.filename}</span>
-				<span className="grey-text">({props.fileSize})</span>
-				<span className="remove-attachment" onClick={()=>props.removeAttachment(props.src)}></span>
-			</a>
-		</div>
-	)
+class FileAttachment extends React.Component{
+	componentDidMount() {
+		this.newData.scrollIntoView({ behavior: "smooth" })
+	}
+	render(){
+		return(
+			<div className="file-attachment-div" ref={(ref) => this.newData = ref}>
+				<a readOnly className="file-attachment" href={this.props.src}>
+					<span className="filename">{this.props.filename}</span>
+					<span className="grey-text">({this.props.fileSize})</span>
+					<span className="remove-attachment" onClick={()=>this.props.removeAttachment(this.props.src)}></span>
+				</a>
+			</div>
+		)
+	}
 }
 
 export default FileAttachment;
